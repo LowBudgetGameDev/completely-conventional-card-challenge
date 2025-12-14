3e9d64ca384e4546b5a065b8924a3893f27043ab
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2025-12-13 21:13:38 -0500

    Fixed card ordering and rotation visual glitch

--- Assets/Scripts/Card/CardRotation.cs
+++ Assets/Scripts/Card/CardRotation.cs
@@ -7,6 +7,21 @@ public class CardRotation : MonoBehaviour
     [SerializeField] private Image frontImage;
     [SerializeField] private Image backImage;
 
+    // Deals with rotation right after being created
+    private void Start()
+    {
+        if ((transform.rotation.eulerAngles.y + 90f) % 360f > 180f)
+        {
+            backImage.gameObject.SetActive(false);
+            frontImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            backImage.gameObject.SetActive(true);
+            frontImage.gameObject.SetActive(false);
+        }
+    }
+
     // Back image will be shown on top by default
     private void Update()
     {
