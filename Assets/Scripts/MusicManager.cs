b0c93bdfa10f66187bd3f0bc1a14d19e73347b27
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2025-12-13 19:18:52 -0500

    Fixed music doubling

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    private static MusicManager instance;
+
     private void Awake()
     {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        instance = this;
+
         DontDestroyOnLoad(gameObject);
     }
 }
